4c7d57d48d9d1da7cb6bb67817833978fb09c17c
Caio Henrique <you@example.com>
2023-06-19 11:58:02 -0300

    primeira parte feita "criar o aula2.html"

---
+++ login.js
@@ -0,0 +1,12 @@
+/* Código de validação de formulário de login */
+
+function Validar(){
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    
+    if(!email || !senha){
+        alert("Campos de preenchimento obrigatório. Favor preencher");
+    }else{
+        alert("Campos preenchidos com sucesso!")
+    }
+}
