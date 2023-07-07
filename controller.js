4e3495b5a6f1874c5dd36b405f98c3150b25e5a5
Caio Henrique <you@example.com>
2023-07-07 10:17:08 -0300

    Alterações gerais em JS, CSS, AULA2

--- controller.js
+++ controller.js
@@ -1,12 +1,21 @@
 /* Código de validação de formulário de login */
 
-function Validar(){
+function acessar(){
     let email = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
     
     if(!email || !senha){
         alert("Campos de preenchimento obrigatório. Favor preencher");
     }else{
-        alert("Campos preenchidos com sucesso!")
+       window.location.href = "aula2.html";
+    }
+}
+
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+var dadosLista = [];
+function salvarUser(){
+    let nomeUser = document.getElementById("nomeUser").value;
+    if(nomeUser){
+        dadosLista.push(nomeUser);
     }
 }
