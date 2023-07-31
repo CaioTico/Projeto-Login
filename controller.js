ff00e6b61568ee196e292e1710fdbb904d04bf11
Caio Henrique <you@example.com>
2023-07-31 11:40:21 -0300

    autalizações de JS

--- controller.js
+++ controller.js
@@ -11,11 +11,25 @@ function acessar(){
     }
 }
 
-// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+/* FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY*/
 var dadosLista = [];
+
 function salvarUser(){
-    let nomeUser = document.getElementById("nomeUser").value;
-    if(nomeUser){
-        dadosLista.push(nomeUser);
+
+  let nomeUser = document.getElementById("nomeUser").value;
+
+   if(nomeUser){
+    dadosLista.push(nomeUser);
+    criarLista();
+}
+}
+
+function criarLista(){
+
+    let tabela = document.getElementById('tabela').innerHTML =  '<tr><th>Nome Usuario</th><th>Ações</th></tr>';
+
+    for(let i = 0;i <= (dadosLista.length - 1);i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
